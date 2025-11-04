3cbe40cffeb7ada95a90176a5e7f402d8492e2b3
Afnan-Afrid-Jamal <afnanafrid6556@gmail.com>
2025-11-04 20:23:22 +0600

    error page design

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -13,7 +13,7 @@ const ErrorPage = () => {
 
 
             <main className="flex-1 flex flex-col items-center justify-center text-center px-4">
-                <FaExclamationTriangle className="text-6xl text-red-500 mb-6 animate-pulse" />
+                <FaExclamationTriangle className="text-6xl text-red-500 mb-6 animate-ping" />
                 <h1 className="text-5xl font-bold text-gray-800 mb-3">Oops! Page Not Found</h1>
                 <p className="text-gray-500 text-lg mb-8">
                     The page you're looking for doesn't exist or has been moved.
@@ -22,7 +22,7 @@ const ErrorPage = () => {
                     to="/home"
                     className="btn flex items-center mt-3 gap-2 bg-gradient-to-r from-[#E59560] to-[#F28C4C] text-white 
                                        font-medium shadow-md hover:from-[#F28C4C] hover:to-[#D77A3D] 
-                                       transition-all duration-300 transform hover:scale-105"
+                                       transition-all duration-300 transform hover:scale-105 animate-pulse hover:animate-none"
                 >
                     Go Back Home
                 </Link>
