839257cde5c47413d505f9938a3c2e83696d10c7
Afnan-Afrid-Jamal <afnanafrid6556@gmail.com>
2025-11-04 20:34:38 +0600

    spinner color changed

--- src/Components/Spinner.jsx
+++ src/Components/Spinner.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Spinner = () => {
     return (
         <div className="fixed inset-0 flex justify-center items-center bg-black/30 z-50">
-            <span className="loading loading-bars loading-xl"></span>
+            <span className="loading loading-bars loading-xl text-[#ffa200]"></span>
+
         </div>
     );
 };
